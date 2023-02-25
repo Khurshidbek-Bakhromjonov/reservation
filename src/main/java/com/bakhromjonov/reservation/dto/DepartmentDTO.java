db683eb5d7df763a12888974a2726372f3fccd53
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-02-25 21:12:00 +0500

    DTOs are done

---
+++ src/main/java/com/bakhromjonov/reservation/dto/DepartmentDTO.java
@@ -0,0 +1,15 @@
+package com.bakhromjonov.reservation.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import java.util.Collection;
+
+@Data
+public class DepartmentDTO {
+
+    private Long id;
+    private String name;
+    @JsonIgnore
+    private Collection<RoomDTO> room;
+}
---
+++ src/main/java/com/bakhromjonov/reservation/dto/UserDTO.java
@@ -0,0 +1,37 @@
+package com.bakhromjonov.reservation.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+
+    @Email
+    @NotBlank(message = "Please enter the email")
+    private String email;
+
+    @Length(max = 30, min = 6)
+    @NotBlank(message = "Please enter the password")
+    private String password;
+
+    @JsonIgnore
+    private Collection<RoleDTO> roles = new ArrayList<>();
+
+    @JsonIgnore
+    private Collection<BookingDTO> bookings;
+}
