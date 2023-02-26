7c23a6369712cb2f611329e6691e7a139057135e
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-02-26 15:51:53 +0500

    EmailSenderService

--- src/main/java/com/bakhromjonov/reservation/service/EmailSenderService.java
+++ src/main/java/com/bakhromjonov/reservation/service/EmailSenderService.java
@@ -1,4 +1,24 @@
 package com.bakhromjonov.reservation.service;
 
-public interface EmailSenderService {
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class EmailSenderService {
+
+    private final JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String body, String subject) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setText(body);
+        simpleMailMessage.setSubject(subject);
+
+        javaMailSender.send(simpleMailMessage);
+    }
 }
