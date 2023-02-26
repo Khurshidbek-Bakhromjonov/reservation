cbbe1db365dffe8c572c698fd04d02011a7ec7d9
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-02-26 10:53:08 +0500

    booking convert is done

--- src/main/java/com/bakhromjonov/reservation/service/BookingService.java
+++ src/main/java/com/bakhromjonov/reservation/service/BookingService.java
@@ -1,4 +1,15 @@
 package com.bakhromjonov.reservation.service;
 
+import com.bakhromjonov.reservation.repositorty.BookingRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class BookingService {
+
+    private final BookingRepository bookingRepository;
+
 }
