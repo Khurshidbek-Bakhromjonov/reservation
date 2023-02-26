994e4ce0416c2c479b40061723e500da271db857
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-02-26 11:54:38 +0500

    RoleService

--- src/main/java/com/bakhromjonov/reservation/mapper/RoleConvert.java
+++ src/main/java/com/bakhromjonov/reservation/mapper/RoleConvert.java
@@ -1,4 +1,31 @@
 package com.bakhromjonov.reservation.mapper;
 
+import com.bakhromjonov.reservation.dto.RoleDTO;
+import com.bakhromjonov.reservation.entity.Role;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class RoleConvert {
+
+    public RoleDTO entityToDto(Role role) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(role, RoleDTO.class);
+    }
+
+    public List<RoleDTO> entityToDto(List<Role> roles) {
+        return roles.stream().map(this::entityToDto).collect(Collectors.toList());
+    }
+
+    public Role dtoToEntity(RoleDTO roleDTO) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(roleDTO, Role.class);
+    }
+
+    public List<Role> dtoToEntity(List<RoleDTO> roleDTOS) {
+        return roleDTOS.stream().map(this::dtoToEntity).collect(Collectors.toList());
+    }
 }
